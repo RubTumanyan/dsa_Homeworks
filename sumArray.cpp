840d1e5b4df1f069ba84bbfd7bb2840022a326b2
RubTumanyan <ruben.tumanyan777@gmail.com>
2025-09-08 11:02:08 -0700

    Update sumArray.cpp

--- sumArray.cpp
+++ sumArray.cpp
@@ -1,5 +1,4 @@
-#include <bits/stdc++.h>
-
+#include <iostream>
 
 
 template<typename T>
@@ -15,11 +14,16 @@ int main() {
     
     int array1[5] = {4,5,2,3,6};
     std::cout << sumArray(array1 , 5);
+    
+    double array2[5] = {4.52 , 5.25 , 2.26 ,3.456};
+    std::cout << sumArray(array2 , 5);
+    
     std::cout << std::endl;
-    std::string array2[3] = {"hello" , "Miki" , "Karen"};
-    std::string result = sumArray(array2 , 3);
+    std::string array3[3] = {"hello" , "Miki" , "Karen"};
+    
+    std::string result = sumArray(array3 , 3);
     std::cout << result;
-    std::cout << "/n" << std::cout << "its work"<< std::endl;
+    std::cout << "\n" << "its work"<< std::endl;
     
     
     
